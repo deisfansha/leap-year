1046f2b3fc1a1e41d0397ca58d05ea0c18d3d5cf
deisfansha <deisfansha8082@gmail.com>
2023-07-25 10:14:00 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int year = input.nextInt();
+        isLeapYear(year);
+    }
+
+    public static void isLeapYear(int year) {
+
+        if(year % 4 == 0 && year %100 != 0 || year %400 == 0){
+            System.out.println("Tahun "+year+" adalah tahun kabisat.");
+        }else {
+            System.out.println("Tahun " +year+" bukan tahun kabisat.");
+        }
+    }
+}

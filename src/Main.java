b81e1030f0b3a924738993a9849eda0244719b14
deisfansha <deisfansha8082@gmail.com>
2023-07-25 10:29:19 +0700

    Menambahkan keterangan masukkan tahun

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.print("Masukkan tahun : ");
         int year = input.nextInt();
         isLeapYear(year);
     }
